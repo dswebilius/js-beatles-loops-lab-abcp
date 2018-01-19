bee2343c6137941d967c736c4475c79f95e36ae9
Dave Swebilius <dswebilius@gmail.com>
2018-01-19 22:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts (array) {
   const facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
-  while 
+  while (facts > 0) {
+    
+  }
 }
 
 function iLoveTheBeatles (num) {
