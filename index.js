284a994aa6f096d7e59ab195aca828f86de4a9d6
Dave Swebilius <dswebilius@gmail.com>
2018-01-19 22:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function theBeatlesPlay (arrayOne, arrayTwo) {
-  var musicians = [];
-  var instruments = [];
-  for (i=0; i )
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
 }
 
 function johnLennonFacts (array) {
